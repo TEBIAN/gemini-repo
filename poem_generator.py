fed07aa153bfe2b930d800f748650620658588f4
Tebian <tebian.abdalgafar@gmail.com>
2025-10-05 20:37:34 +0400

    Update poem generator to be random

--- poem_generator.py
+++ poem_generator.py
@@ -1,13 +1,28 @@
-'''
+import random
+
 def generate_poem():
-    poem = """
-    يا بلادي
-    فيك طبيعة ساحرة وجمال رباني
-    فيك النيل العظيم وسهولك الخضراء
-    فيك الكرم والجود وأهلك الطيبين
-    يا سودان يا وطني
-    """
-    return poem
+    poems = [
+        """
+        يا بلادي
+        فيك طبيعة ساحرة وجمال رباني
+        فيك النيل العظيم وسهولك الخضراء
+        فيك الكرم والجود وأهلك الطيبين
+        يا سودان يا وطني
+        """,
+        """
+        يا وطني يا بلد
+        يا حبيب يا سند
+        فيك الخير اتولد
+        وبيك الخير انوجد
+        """,
+        """
+        سودانا فوق
+        فوق السحاب
+        رمز الشموخ
+        رمز الإباء
+        """
+    ]
+    return random.choice(poems)
 
 def write_poem_to_file(poem, filename="poem.txt"):
     with open(filename, "w", encoding="utf-8") as f:
@@ -17,4 +32,3 @@ if __name__ == "__main__":
     poem = generate_poem()
     write_poem_to_file(poem)
     print(f"Poem written to poem.txt")
-'''
